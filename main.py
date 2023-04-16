6bbd12e273a757b0cd04a620506a76da3a4eb4e3
lyamlim97 <lyamlim97@gmail.com>
2023-04-17 05:19:19 +0800

    add constructor with assertions

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    def __init__(self, name: str, price:float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
 
-print(item1.calculate_total_price(item1.price, item1.quantity))
-
-item2 = Item()
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
-
-print(item2.calculate_total_price(item2.price, item2.quantity))
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
